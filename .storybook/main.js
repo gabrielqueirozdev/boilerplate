30b4f243468305b89624c55bfb51cd66851a761b
Gabriel Queiroz <gabriel.queiroz@softecnologia.com.br>
2022-02-21 14:51:20 -0300

    25. Usando Storybook Essentials e Controls

--- .storybook/main.js
+++ .storybook/main.js
@@ -4,8 +4,7 @@ module.exports = {
     "../src/**/stories.@(js|jsx|ts|tsx)"
   ],
   "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-actions"
+    "@storybook/addon-essentials"
   ],
   "framework": "@storybook/react"
 }
